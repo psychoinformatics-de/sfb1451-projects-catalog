aeadc80d3ee6ad3b2d3aebe36e22972cf7f1d44b
Michał Szczepanik <m.szczepanik@fz-juelich.de>
2023-04-11 15:27:29 +0200

    Add a Python script for project metadata extraction
    The script is based around meta_conduct + catalog translate. Two new
    directories, code and metadata are created (the latter to store
    extracted and translated metadata filesm bypassing meta_add).

---
+++ code/extract_project.py
@@ -0,0 +1,61 @@
+"""Extract metadata from a project's superdataset
+
+Conducts project_extract_pipeline.json and translates the
+result. Extracted and translated metadata are written to the specified
+directory as two separate files (project.jsonl, project.cat.jsonl).
+
+"""
+
+import argparse
+import json
+from pathlib import Path
+from uuid import UUID
+
+from datalad.api import(
+    catalog,
+    meta_conduct,
+)
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if type(obj) is UUID:
+            return str(obj)
+        return super.default(obj)
+
+
+# allow manual specification
+parser = argparse.ArgumentParser()
+parser.add_argument("superds", type=Path, help="Dataset with all the projects")
+parser.add_argument("outdir", type=Path, help="Metadata output directory")
+parser.add_argument("project", help="Project name")
+args = parser.parse_args()
+
+# extraction
+res = meta_conduct(
+    configuration=str(Path(__file__).parent.joinpath("project_extract_pipeline.json")),
+    arguments=[
+        f"traverser.top_level_dir={args.superds / args.project}",
+    ],
+    return_type="item-or-list",
+)
+
+metadata = res["pipeline_data"]["result"]["metadata"]
+
+extracted_path = args.outdir.joinpath(f"{args.project}.jsonl")
+
+with extracted_path.open("w") as json_file:
+    for md in metadata:
+        assert md["state"] == "SUCCESS"  # crude check
+        json.dump(md["metadata_record"], json_file, cls=MyEncoder)
+        json_file.write("\n")
+
+# translation
+translated_name = f"{extracted_path.stem}.cat.jsonl"
+translated_path = args.outdir.joinpath(translated_name)
+
+with translated_path.open("w") as json_file:
+    for res in catalog("translate", metadata=extracted_path, return_type="generator"):
+        assert res["status"] == "ok"  # crude check
+        json.dump(res["translated_metadata"], json_file)
+        json_file.write("\n")
+
