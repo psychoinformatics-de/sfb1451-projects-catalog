a89e2902f60cf8efa80554ae8287d9bb039a7167
Michał Szczepanik <m.szczepanik@fz-juelich.de>
2023-10-11 21:06:54 +0200

    Create dedicated script for superdataset extraction
    There is a noticeable duplication with extract_selected, where
    extract_superdataset is essentially extract_selected with a fixed
    extractor list and an additional catalog_set call. However, for usage
    and CLI clarity, the separation feels welcome.
    
    Shared functions are moved to utils.

--- code/extract_superdataset.py
+++ code/extract_superdataset.py
@@ -4,38 +4,26 @@ from pathlib import Path
 
 from datalad.api import (
     catalog_add,
+    catalog_set,
     catalog_translate,
     meta_extract,
 )
-
 
 from utils import MyEncoder, postprocess
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("dataset", type=Path, help="Dataset to extract from")
+parser.add_argument("dataset", type=Path, help="Superdataset to extract from")
 parser.add_argument("outdir", type=Path, help="Metadata output directory")
 parser.add_argument("-c", "--catalog", type=Path, help="Catalog to add metadata to")
-parser.add_argument(
-    "--filename",
-    help="Use this file name instead of deriving from folder names",
-)
-parser.add_argument("extractors", metavar="name", nargs="+", help="Extractors to use")
-
 args = parser.parse_args()
 
-# obtain or generate name for metadata file
-if args.filename is not None:
-    extracted_path = args.outdir.joinpath(args.filename)
-else:
-    # we assume it's a project's subdataset
-    ds_name = args.dataset.name
-    project_name = args.dataset.parent.name
-    extracted_path = args.outdir.joinpath(f"{project_name}_{ds_name}.jsonl")
+extracted_path = args.outdir.joinpath(f"{args.dataset.name}.jsonl")
+translated_path = extracted_path.with_suffix(".cat.jsonl")
 
 # extract
 with extracted_path.open("w") as json_file:
-    for extractor_name in args.extractors:
+    for extractor_name in ("metalad_core", "metalad_studyminimeta"):
         res = meta_extract(
             extractorname=extractor_name,
             dataset=args.dataset,
@@ -46,11 +34,15 @@ with extracted_path.open("w") as json_file:
         json.dump(res["metadata_record"], json_file, cls=MyEncoder)
         json_file.write("\n")
 
-# translate + postprocess
-translated_path = extracted_path.with_suffix(".cat.jsonl")
 
+# translate + postprocess
 with translated_path.open("w") as json_file:
-    for res in catalog_translate(metadata=extracted_path, catalog=None, return_type="generator"):
+    for res in catalog_translate(
+        metadata=extracted_path,
+        catalog=None,
+        return_type="generator",
+        result_renderer="disabled",
+    ):
         assert res["status"] == "ok"  # crude check
         metadata_item = postprocess(res)
         json.dump(metadata_item, json_file)
@@ -61,5 +53,13 @@ if args.catalog is not None:
     catalog_add(
         catalog=args.catalog,
         metadata=translated_path,
-        config_file=args.catalog / "config.json",
+        config_file=args.add_catalog / "config.json",
+    )
+
+    catalog_set(
+        catalog=args.catalog,
+        property="home",
+        dataset_id=metadata_item["dataset_id"],
+        dataset_version=metadata_item["dataset_version"],
+        reckless="overwrite",
     )
